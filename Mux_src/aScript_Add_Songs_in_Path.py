307a6e35632f11cb010fe321f80cd7e1a1b68629
rduvalwa5 <rduvalwa5@hotmail.com>
2017-05-25 15:14:44 -0700

    add songs

--- Mux_src/aScript_Add_Songs_in_Path.py
+++ Mux_src/aScript_Add_Songs_in_Path.py
@@ -7,10 +7,10 @@ from Music_Get_Functions import musicGet_Functions
         
 if __name__ == "__main__" :       
     mux = musicGet_Functions()
-    myPath = "/Users/rduvalwa2/music/iTunes/iTunes Music/Music/Bread/The Best Of Bread"
+    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Grateful Dead/Anthem Of The Sun/"
 #    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Kenny Rogers & The First Edition/Anthology Kenny Rogers & The First Edition"
-    album = "The Best Of Bread"
-    artist = "Bread"
+    album = "Anthem Of The Sun"
+    artist = "Grateful Dead"
     genre = "Rock"
     inType = "CD"    
     mux.add_songs_in_path(myPath, album, artist, genre, inType)   
