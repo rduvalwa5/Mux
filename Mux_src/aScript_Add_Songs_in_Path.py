c09d17ce1a77a51e7d0ab7066fb0956a6915c492
rduvalwa5 <rduvalwa5@hotmail.com>
2017-06-28 17:00:49 -0700

    add music

--- Mux_src/aScript_Add_Songs_in_Path.py
+++ Mux_src/aScript_Add_Songs_in_Path.py
@@ -7,10 +7,10 @@ from Music_Get_Functions import musicGet_Functions
         
 if __name__ == "__main__" :       
     mux = musicGet_Functions(True)
-    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Yes/The Yes Album"
+    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Pink Floyd/More_ Original Motion Picture Soundtrack"
 #    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Kenny Rogers & The First Edition/Anthology Kenny Rogers & The First Edition"
-    album = "The Yes Album"
-    artist = "Yes"
-    genre = "Rock"
+    album = "More"
+    artist = "Pink Floyd"
+    genre = "Soundtrack"
     inType = "CD"    
     mux.add_songs_in_path(myPath, album, artist, genre, inType)   
