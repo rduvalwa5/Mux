1bbe4279a6f28301ff48136e75f66ecec1273f6e
rduvalwa5 <rduvalwa5@hotmail.com>
2017-10-31 22:36:25 -0700

    add music

--- Mux_src/aScript_Add_Songs_in_Path.py
+++ Mux_src/aScript_Add_Songs_in_Path.py
@@ -7,10 +7,10 @@ from Music_Get_Functions import musicGet_Functions
         
 if __name__ == "__main__" :       
     mux = musicGet_Functions(True)
-    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Elton John/The One"
+    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Linda Ronstadt & Emmylou Harris/Western Wall_ The Tucson Sessions"
 #    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Kenny Rogers & The First Edition/Anthology Kenny Rogers & The First Edition"
-    album = "The One"
-    artist = "Elton John"
-    genre = "Rock"
+    album = "Western Wall: The Tucson Sessions"
+    artist = "Linda Ronstadt & Emmylou Harris"
+    genre = "Country"
     inType = "CD"    
     mux.add_songs_in_path(myPath, album, artist, genre, inType)   
