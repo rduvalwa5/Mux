d4488e8bac510a8fe70d5843e5b77b303fe3bbff
rduvalwa5 <rduvalwa5@hotmail.com>
2018-03-27 00:14:37 -0700

    add songs Lester Young fix test

--- Mux_src/Test_Results.py
+++ Mux_src/Test_Results.py
@@ -9,7 +9,7 @@ Classical    59
 Country    893
 Easy Listening    28
 Folk    677
-Jazz    781
+Jazz    787
 Pop    548
 Regae    23
 Rock    4632
@@ -22,14 +22,14 @@ TexMex    202
 '''
 
 cover_count = 532
-songs_count = 9041
+songs_count = 9047
 artist_count = 491
-artist_albums_count = 971
+artist_albums_count = 972
 folk_songs = 677
 folk_albums = 51
 get_max_index_artist = 491
-get_max_index_albums = 972
-get_max_index_songs = 9042
+get_max_index_albums = 973
+get_max_index_songs = 9048
 #get_song = (1134, 'OSXAir.home', '/Users/rduvalwa2/Music/iTunes/iTunes Music/Music', 'Chuck Berry', 'The Best of Chuck Berry', '08 Johnny B. Goode.mp3', 'Rock', 'Vinyl', 1)
 get_song = ((2389, 'OSXAir.home', '/Users/rduvalwa2/Music/iTunes/iTunes Music/Music', 'Grateful Dead', 'The Grateful Dead (Skull & Roses)', '09 Johnny B. Goode.mp3', 'Rock', 'Download', 1), (4044, 'OSXAir.home', '/Users/rduvalwa2/Music/iTunes/iTunes Music/Music', 'Chuck Berry', 'The Best of Chuck Berry', '08 Johnny B. Goode.mp3', 'Rock', 'Vinyl', 1))
 get_album = (188, 'Ten Years After', 'A Space In Time', 'Blues', 'Download', None, None)
@@ -39,9 +39,9 @@ get_artist_albums_songs = (("03 I'd Love to Change the World.m4p",), ('02 Here T
 
 get_artist_songs = (('08 Johnny B. Goode.mp3', 'The Best of Chuck Berry'), ('02 Roll Over Beethoven.mp3', 'The Best of Chuck Berry'), ('10 Sweet Little Sixteen.mp3', 'The Best of Chuck Berry'), ('09 Brown Eyed Handsome Man.mp3', 'The Best of Chuck Berry'), ('07 Maybelline.mp3', 'The Best of Chuck Berry'), ('03 Rock And Roll Music.mp3', 'The Best of Chuck Berry'), ('05 No Particular Place To Go.mp3', 'The Best of Chuck Berry'), ('06 My Ding A Ling.mp3', 'The Best of Chuck Berry'), ('01 School Days.mp3', 'The Best of Chuck Berry'), ("04 Reelin' and Rockin'.mp3", 'The Best of Chuck Berry')) 
 genreList = [('Alternative', 0), ('BlueGrass', 179), ('Blues', 785), ('Classical', 59), ('Country', 893), \
-            ('Easy Listening', 28), ('Folk', 677), ('Holiday', 0), ('Jazz', 781), ('Latino', 0), ('Pop', 548), ('Regae', 23), \
+            ('Easy Listening', 28), ('Folk', 677), ('Holiday', 0), ('Jazz', 787), ('Latino', 0), ('Pop', 548), ('Regae', 23), \
             ('Rock', 4632), ('RockaBilly', 32), ('Soul', 198), ('Talk', 1), ('TestGenre', 3), ('TexMex', 202), \
             ('Traditional', 0), ('World', 0)]
-typeList = [('CD', 3807), ('Download', 3392), ('Tape', 24), ('TestType', 3), ('Vinyl', 1815)]
+typeList = [('CD', 3807), ('Download', 3392), ('Tape', 24), ('TestType', 3), ('Vinyl', 1821)]
 genresList = (('Rock', 1), ('Alternative', 7), ('BlueGrass', 8), ('Blues', 9), ('Classical', 10), ('Country', 11), ('Folk', 12), ('Holiday', 13), ('Jazz', 14), ('Latino', 15), ('Pop', 16), ('Regae', 17), ('RockaBilly', 18), ('Soul', 19), ('Talk', 20), ('TestGenre', 21), ('TexMex', 22), ('Traditional', 23), ('World', 24), ('NewGenre', 25), ('Easy Listening', 26), ('Classic', 27))
 
