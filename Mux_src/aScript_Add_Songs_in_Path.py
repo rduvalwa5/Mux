c407de55c313b257fca6581c3f72e39b16e1d472
rduvalwa5 <rduvalwa5@hotmail.com>
2018-03-27 00:03:41 -0700

    add Lester Young

--- Mux_src/aScript_Add_Songs_in_Path.py
+++ Mux_src/aScript_Add_Songs_in_Path.py
@@ -7,12 +7,12 @@ from Music_Get_Functions import musicGet_Functions
         
 if __name__ == "__main__" :       
     mux = musicGet_Functions(True)
-    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Ringo Starr/Postcards From Paradise"    
-    album = "Postcards From Paradise"
-    artist = "Ringo Starr"
-    genre = "Rock"
-    inType = "CD"    
-    mux.add_songs_in_path(myPath, album, artist, genre, inType)
-#    mux.add_album(album, artist, genre, inType)
+    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Lester Young/Pres"    
+    album = "Pres"
+    artist = "Lester Young"
+    genre = "Jazz"
+    inType = "Vinyl"    
+#    mux.add_songs_in_path(myPath, album, artist, genre, inType)
+    mux.add_album(album, artist, genre, inType)
 #    mux.add_artist(artist, genre)
     
