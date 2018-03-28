e1ff43942aab1f4529108cc965526e205aead14b
rduvalwa5 <rduvalwa5@hotmail.com>
2018-03-27 22:53:38 -0700

    add songs Leonard Cohen in this batch

--- Mux_src/aScript_Add_Songs_in_Path.py
+++ Mux_src/aScript_Add_Songs_in_Path.py
@@ -7,12 +7,12 @@ from Music_Get_Functions import musicGet_Functions
         
 if __name__ == "__main__" :       
     mux = musicGet_Functions(True)
-    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Lester Young/Pres"    
-    album = "Pres"
-    artist = "Lester Young"
-    genre = "Jazz"
-    inType = "Vinyl"    
-#    mux.add_songs_in_path(myPath, album, artist, genre, inType)
-    mux.add_album(album, artist, genre, inType)
+    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Leonard Cohen/Popular Problems"    
+    album = "Popular Problems"
+    artist = "Leonard Cohen"
+    genre = "Folk"
+    inType = "CD"    
+    mux.add_songs_in_path(myPath, album, artist, genre, inType)
+#    mux.add_album(album, artist, genre, inType)
 #    mux.add_artist(artist, genre)
     
