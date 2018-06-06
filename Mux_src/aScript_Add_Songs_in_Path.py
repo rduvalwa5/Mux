58e9a0d1919c160fdb2e2ee2fe3ac5fd9afc3591
rduvalwa5 <rduvalwa5@hotmail.com>
2018-06-06 13:38:12 -0700

    add new music

--- Mux_src/aScript_Add_Songs_in_Path.py
+++ Mux_src/aScript_Add_Songs_in_Path.py
@@ -7,12 +7,12 @@ from Music_Get_Functions import musicGet_Functions
         
 if __name__ == "__main__" :       
     mux = musicGet_Functions(True)
-    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Santana/Santana IV"    
-    album = "Santana IV"
-    artist = "Santana"
-    genre = "TexMex"
+    myPath = "/Users/rduvalwa2/music/iTunes/iTunes Music/Music/Isaac Hayes/Isaac Hayes Greatest Hit Singles"    
+    album = "Isaac Hayes Greatest Hit Singles"
+    artist = "Isaac Hayes"
+    genre = "R&B"
     inType = "CD"    
-    mux.add_songs_in_path(myPath, album, artist, genre, inType)
-#    mux.add_album(album, artist, genre, inType)
-#    mux.add_artist(artist, genre)
+#    mux.add_songs_in_path(myPath, album, artist, genre, inType)
+    mux.add_album(album, artist, genre, inType)
+    mux.add_artist(artist, genre)
     
