8603eb0425e95603522b4e25e9b11b4e09e04d66
rduvalwa5 <rduvalwa5@hotmail.com>
2018-07-26 23:32:45 -0700

    add music

--- Mux_src/aScript_Add_Songs_in_Path.py
+++ Mux_src/aScript_Add_Songs_in_Path.py
@@ -7,12 +7,12 @@ from Music_Get_Functions import musicGet_Functions
         
 if __name__ == "__main__" :       
     mux = musicGet_Functions(True)
-    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Keith Richards/Crosseyed Heart"    
-    album = "Keep On Running"
-    artist = "The Spencer Davis Group"
+    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/The Beatles/Let It Be... Naked [Disc 1]"    
+    album = "The Beatles/Let It Be... Naked [Disc 1]"
+    artist = "The Beatles"
     genre = "Rock"
-    inType = "Download"    
+    inType = "CD"    
 #    mux.add_songs_in_path(myPath, album, artist, genre, inType)
-#    mux.add_album(album, artist, genre, inType)
-    mux.add_artist(artist, genre)
+    mux.add_album(album, artist, genre, inType)
+#    mux.add_artist(artist, genre)
     
