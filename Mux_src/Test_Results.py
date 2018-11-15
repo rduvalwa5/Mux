a1e6ab642bda8983c84d4662bf745230e107a238
rduvalwa5 <rduvalwa5@hotmail.com>
2018-11-14 22:26:18 -0800

    add music Rod Stewart Sonics Talking Heads etc

--- Mux_src/Test_Results.py
+++ Mux_src/Test_Results.py
@@ -4,7 +4,7 @@ Created on Apr 25, 2017
 @author: rduvalwa2
 
 BlueGrass    179
-Blues    785
+Blues    803
 Classical    59
 Country    964
 Easy Listening    28
@@ -21,15 +21,15 @@ TexMex    218
 
 '''
 
-cover_count = 570
-songs_count = 10258
-artist_count = 512
-artist_albums_count = 1069
+cover_count = 585
+songs_count = 10488
+artist_count = 518
+artist_albums_count = 1090
 folk_songs = 677
-folk_albums = 50
-get_max_index_artist = 513
-get_max_index_albums = 1070
-get_max_index_songs = 10261
+folk_albums = 51
+get_max_index_artist = 519
+get_max_index_albums = 1091
+get_max_index_songs = 10492
 #get_song = (1134, 'OSXAir.home', '/Users/rduvalwa2/Music/iTunes/iTunes Music/Music', 'Chuck Berry', 'The Best of Chuck Berry', '08 Johnny B. Goode.mp3', 'Rock', 'Vinyl', 1)
 get_song = ((2389, 'OSXAir.home', '/Users/rduvalwa2/Music/iTunes/iTunes Music/Music', 'Grateful Dead', 'The Grateful Dead (Skull & Roses)', '09 Johnny B. Goode.mp3', 'Rock', 'Download', 1), (4044, 'OSXAir.home', '/Users/rduvalwa2/Music/iTunes/iTunes Music/Music', 'Chuck Berry', 'The Best of Chuck Berry', '08 Johnny B. Goode.mp3', 'Rock', 'Vinyl', 1))
 get_album = (188, 'Ten Years After', 'A Space In Time', 'Blues', 'Download', None, None)
@@ -38,10 +38,10 @@ get_artist_albums = ((185, 'Ten Years After', 'Stonedhenge (Re-Presents)', 'Blue
 get_artist_albums_songs = (("03 I'd Love to Change the World.m4p",), ('02 Here They Come.m4p',), ("05 Baby Won't You Let Me Rock 'N' Roll You.m4p",), ('01 One of These Days.m4p',), ('10 Uncle Jam.m4p',), ('04 Over the Hill.m4p',), ('07 Let the Sky Fall.m4p',), ("09 I've Been There Too.m4p",), ('06 Once There Was a Time.m4p',), ('08 Hard Monkeys.m4p',))
 
 get_artist_songs = (('08 Johnny B. Goode.mp3', 'The Best of Chuck Berry'), ('02 Roll Over Beethoven.mp3', 'The Best of Chuck Berry'), ('10 Sweet Little Sixteen.mp3', 'The Best of Chuck Berry'), ('09 Brown Eyed Handsome Man.mp3', 'The Best of Chuck Berry'), ('07 Maybelline.mp3', 'The Best of Chuck Berry'), ('03 Rock And Roll Music.mp3', 'The Best of Chuck Berry'), ('05 No Particular Place To Go.mp3', 'The Best of Chuck Berry'), ('06 My Ding A Ling.mp3', 'The Best of Chuck Berry'), ('01 School Days.mp3', 'The Best of Chuck Berry'), ("04 Reelin' and Rockin'.mp3", 'The Best of Chuck Berry')) 
-genreList = [('Alternative', 0), ('BlueGrass', 179), ('Blues', 785), ('Classical', 59), ('Country', 964), \
-            ('Easy Listening', 28), ('Folk', 677), ('Holiday', 0), ('Jazz', 797), ('Latino', 0), ('Pop', 679), ('Regae', 23), \
-            ('Rock', 5489), ('RockaBilly', 32), ('Soul', 32), ('Talk', 1), ('TestGenre', 3), ('TexMex', 218), \
+genreList = [('Alternative', 0), ('BlueGrass', 179), ('Blues', 803), ('Classical', 59), ('Country', 964), \
+            ('Easy Listening', 28), ('Folk', 677), ('Holiday', 0), ('Jazz', 860), ('Latino', 0), ('Pop', 679), ('Regae', 23), \
+            ('Rock', 5638), ('RockaBilly', 32), ('Soul', 32), ('Talk', 1), ('TestGenre', 3), ('TexMex', 218), \
             ('Traditional', 0), ('World', 0)]
-typeList = [('CD', 4998), ('Download', 3412), ('Tape', 24), ('TestType', 3), ('Vinyl', 1821)]
+typeList = [('CD', 5226), ('Download', 3414), ('Tape', 24), ('TestType', 3), ('Vinyl', 1821)]
 genresList = (('Rock', 1), ('Alternative', 7), ('BlueGrass', 8), ('Blues', 9), ('Classical', 10), ('Country', 11), ('Folk', 12), ('Holiday', 13), ('Jazz', 14), ('Latino', 15), ('Pop', 16), ('Regae', 17), ('RockaBilly', 18), ('Soul', 19), ('Talk', 20), ('TestGenre', 21), ('TexMex', 22), ('Traditional', 23), ('World', 24), ('NewGenre', 25), ('Easy Listening', 26), ('Classic', 27),('R&B', 28))
 
--- Mux_src/aScript_Add_Songs_in_Path.py
+++ Mux_src/aScript_Add_Songs_in_Path.py
@@ -7,12 +7,12 @@ from Music_Get_Functions import musicGet_Functions
         
 if __name__ == "__main__" :       
     mux = musicGet_Functions(True)
-    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/The Beatles/Let It Be... Naked [Disc 1]"    
-    album = "The Beatles/Let It Be... Naked [Disc 1]"
-    artist = "The Beatles"
+    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Rod Stewart/Gold [Disc 2]"    
+    album = "Gold [Disc 2]"
+    artist = "Rod Stewart"
     genre = "Rock"
     inType = "CD"    
-#    mux.add_songs_in_path(myPath, album, artist, genre, inType)
-    mux.add_album(album, artist, genre, inType)
+    mux.add_songs_in_path(myPath, album, artist, genre, inType)
+#    mux.add_album(album, artist, genre, inType)
 #    mux.add_artist(artist, genre)
     
