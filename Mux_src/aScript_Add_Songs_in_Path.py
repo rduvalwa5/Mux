447d3f5dd6c7e0f4e491151e5876f006d29f45bc
rduvalwa5 <rduvalwa5@hotmail.com>
2018-11-25 16:27:14 -0800

    correct test results

--- Mux_src/aScript_Add_Songs_in_Path.py
+++ Mux_src/aScript_Add_Songs_in_Path.py
@@ -7,12 +7,12 @@ from Music_Get_Functions import musicGet_Functions
         
 if __name__ == "__main__" :       
     mux = musicGet_Functions(True)
-    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Rod Stewart/Gold [Disc 2]"    
-    album = "Gold [Disc 2]"
-    artist = "Rod Stewart"
+    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Spanky & Our Gang/The Best Of Spanky & Our Gang 20th Century Masters The Millennium Collection"    
+    album = "The Best Of Spanky & Our Gang 20th Century Masters The Millennium Collection"
+    artist = "Spanky & Our Gang"
     genre = "Rock"
     inType = "CD"    
-    mux.add_songs_in_path(myPath, album, artist, genre, inType)
-#    mux.add_album(album, artist, genre, inType)
-#    mux.add_artist(artist, genre)
+#    mux.add_songs_in_path(myPath, album, artist, genre, inType)
+    mux.add_album(album, artist, genre, inType)
+    mux.add_artist(artist, genre)
     
