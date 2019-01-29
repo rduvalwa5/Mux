aa204343f95cc973a1eb2572f55af470038c6620
rduvalwa5 <rduvalwa5@hotmail.com>
2019-01-28 21:30:16 -0800

    configure album covers

--- Mux_src/aScript_Add_Songs_in_Path.py
+++ Mux_src/aScript_Add_Songs_in_Path.py
@@ -7,12 +7,12 @@ from Music_Get_Functions import musicGet_Functions
         
 if __name__ == "__main__" :       
     mux = musicGet_Functions(True)
-    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Spanky & Our Gang/The Best Of Spanky & Our Gang 20th Century Masters The Millennium Collection"    
-    album = "The Best Of Spanky & Our Gang 20th Century Masters The Millennium Collection"
-    artist = "Spanky & Our Gang"
-    genre = "Rock"
+    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Tower of Power/Great American Soulbook"    
+    album = "Great American Soulbook"
+    artist = "Tower of Power"
+    genre = "R&B"
     inType = "CD"    
 #    mux.add_songs_in_path(myPath, album, artist, genre, inType)
     mux.add_album(album, artist, genre, inType)
-    mux.add_artist(artist, genre)
+#    mux.add_artist(artist, genre)
     
