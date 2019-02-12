49902649cff8927612d5395d94bf9681e5e6c150
rduvalwa5 <rduvalwa5@hotmail.com>
2019-02-12 11:31:53 -0800

    fix test

--- Mux_src/Test_Results.py
+++ Mux_src/Test_Results.py
@@ -6,11 +6,11 @@ Created on Apr 25, 2017
 BlueGrass    179
 Blues    803
 Classical    59
-Country    964
+Country    974
 Easy Listening    28
 Folk    677
 Jazz    797
-Pop    679
+Pop    691
 Regae    23
 Rock    5148
 RockaBilly    32
@@ -21,15 +21,15 @@ TexMex    218
 
 '''
 
-cover_count = 586
-songs_count = 10549
-artist_count = 520
-artist_albums_count = 1096
+cover_count = 595
+songs_count = 10674
+artist_count = 523
+artist_albums_count = 1105
 folk_songs = 677
 folk_albums = 51
-get_max_index_artist = 521
-get_max_index_albums = 1097
-get_max_index_songs = 10553
+get_max_index_artist = 524
+get_max_index_albums = 1109
+get_max_index_songs = 10678
 #get_song = (1134, 'OSXAir.home', '/Users/rduvalwa2/Music/iTunes/iTunes Music/Music', 'Chuck Berry', 'The Best of Chuck Berry', '08 Johnny B. Goode.mp3', 'Rock', 'Vinyl', 1)
 get_song = ((2389, 'OSXAir.home', '/Users/rduvalwa2/Music/iTunes/iTunes Music/Music', 'Grateful Dead', 'The Grateful Dead (Skull & Roses)', '09 Johnny B. Goode.mp3', 'Rock', 'Download', 1), (4044, 'OSXAir.home', '/Users/rduvalwa2/Music/iTunes/iTunes Music/Music', 'Chuck Berry', 'The Best of Chuck Berry', '08 Johnny B. Goode.mp3', 'Rock', 'Vinyl', 1))
 get_album = (188, 'Ten Years After', 'A Space In Time', 'Blues', 'Download', None, None)
@@ -38,10 +38,10 @@ get_artist_albums = ((185, 'Ten Years After', 'Stonedhenge (Re-Presents)', 'Blue
 get_artist_albums_songs = (("03 I'd Love to Change the World.m4p",), ('02 Here They Come.m4p',), ("05 Baby Won't You Let Me Rock 'N' Roll You.m4p",), ('01 One of These Days.m4p',), ('10 Uncle Jam.m4p',), ('04 Over the Hill.m4p',), ('07 Let the Sky Fall.m4p',), ("09 I've Been There Too.m4p",), ('06 Once There Was a Time.m4p',), ('08 Hard Monkeys.m4p',))
 
 get_artist_songs = (('08 Johnny B. Goode.mp3', 'The Best of Chuck Berry'), ('02 Roll Over Beethoven.mp3', 'The Best of Chuck Berry'), ('10 Sweet Little Sixteen.mp3', 'The Best of Chuck Berry'), ('09 Brown Eyed Handsome Man.mp3', 'The Best of Chuck Berry'), ('07 Maybelline.mp3', 'The Best of Chuck Berry'), ('03 Rock And Roll Music.mp3', 'The Best of Chuck Berry'), ('05 No Particular Place To Go.mp3', 'The Best of Chuck Berry'), ('06 My Ding A Ling.mp3', 'The Best of Chuck Berry'), ('01 School Days.mp3', 'The Best of Chuck Berry'), ("04 Reelin' and Rockin'.mp3", 'The Best of Chuck Berry')) 
-genreList = [('Alternative', 0), ('BlueGrass', 179), ('Blues', 803), ('Classical', 59), ('Country', 964), \
-            ('Easy Listening', 28), ('Folk', 677), ('Holiday', 0), ('Jazz', 860), ('Latino', 0), ('Pop', 679), ('Regae', 23), \
-            ('Rock', 5699), ('RockaBilly', 32), ('Soul', 32), ('Talk', 1), ('TestGenre', 3), ('TexMex', 218), \
+genreList = [('Alternative', 0), ('BlueGrass', 179), ('Blues', 803), ('Classical', 59), ('Country', 974), \
+            ('Easy Listening', 28), ('Folk', 677), ('Holiday', 0), ('Jazz', 860), ('Latino', 0), ('Pop', 691), ('Regae', 23), \
+            ('Rock', 5790), ('RockaBilly', 32), ('Soul', 32), ('Talk', 1), ('TestGenre', 3), ('TexMex', 218), \
             ('Traditional', 0), ('World', 0)]
-typeList = [('CD', 5287), ('Download', 3414), ('Tape', 24), ('TestType', 3), ('Vinyl', 1821)]
+typeList = [('CD', 5412), ('Download', 3414), ('Tape', 24), ('TestType', 3), ('Vinyl', 1821)]
 genresList = (('Rock', 1), ('Alternative', 7), ('BlueGrass', 8), ('Blues', 9), ('Classical', 10), ('Country', 11), ('Folk', 12), ('Holiday', 13), ('Jazz', 14), ('Latino', 15), ('Pop', 16), ('Regae', 17), ('RockaBilly', 18), ('Soul', 19), ('Talk', 20), ('TestGenre', 21), ('TexMex', 22), ('Traditional', 23), ('World', 24), ('NewGenre', 25), ('Easy Listening', 26), ('Classic', 27),('R&B', 28))
 
