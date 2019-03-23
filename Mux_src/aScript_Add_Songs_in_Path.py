bbf02f3cff4e661d38d1bd35ee9962b9d8de862d
rduvalwa5 <rduvalwa5@hotmail.com>
2019-03-23 00:16:23 -0700

    add music

--- Mux_src/aScript_Add_Songs_in_Path.py
+++ Mux_src/aScript_Add_Songs_in_Path.py
@@ -7,12 +7,11 @@ from Music_Get_Functions import musicGet_Functions
         
 if __name__ == "__main__" :       
     mux = musicGet_Functions(True)
-    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Tower of Power/Great American Soulbook"    
-    album = "Great American Soulbook"
-    artist = "Tower of Power"
-    genre = "R&B"
-    inType = "CD"    
-#    mux.add_songs_in_path(myPath, album, artist, genre, inType)
-    mux.add_album(album, artist, genre, inType)
+    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Neil Warner/Man Of La Mancha"    
+    album = "Man Of La Mancha"
+    artist = "Neil Warner"
+    genre = "Sound Track"
+    inType = "Vinyl"    
+    mux.add_songs_in_path(myPath, album, artist, genre, inType)
+#    mux.add_album(album, artist, genre, inType)
 #    mux.add_artist(artist, genre)
-    
