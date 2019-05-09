9ec447b3c21a696e981eb76699aa36cc67bc0939
rduvalwa5 <rduvalwa5@hotmail.com>
2019-05-08 23:43:03 -0700

    add music

--- Mux_src/aScript_Add_Songs_in_Path.py
+++ Mux_src/aScript_Add_Songs_in_Path.py
@@ -7,10 +7,10 @@ from Music_Get_Functions import musicGet_Functions
         
 if __name__ == "__main__" :       
     mux = musicGet_Functions(True)
-    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/The Doors/The Soft Parade [40th Anniversary Edition]"    
-    album = "The Soft Parade [40th Anniversary Edition]"
-    artist = "The Doors"
-    genre = "Rock"
+    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/The Spinners/The Definitive Soul Collection Of The Spinners [Disc 1]"    
+    album = "The Definitive Soul Collection Of The Spinners [Disc 2]"
+    artist = "The Spinners"
+    genre = "R&B"
     inType = "CD"    
 #    mux.add_songs_in_path(myPath, album, artist, genre, inType)
     mux.add_album(album, artist, genre, inType)
