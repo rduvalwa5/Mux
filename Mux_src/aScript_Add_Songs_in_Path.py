a7a67b4a9fa0cc5576bde0f50278b7b8a6234b96
rduvalwa5 <rduvalwa5@hotmail.com>
2019-06-24 23:08:55 -0700

    add music

--- Mux_src/aScript_Add_Songs_in_Path.py
+++ Mux_src/aScript_Add_Songs_in_Path.py
@@ -7,9 +7,9 @@ from Music_Get_Functions import musicGet_Functions
         
 if __name__ == "__main__" :       
     mux = musicGet_Functions(True)
-    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Bruce Springsteen/Greetings From Asbury Park, N.J_"    
-    album = "Greetings From Asbury Park, N.J_"
-    artist = "Bruce Springsteen"
+    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Neil Young/Tonight's The Night"    
+    album = "Tonight's The Night"
+    artist = "Neil Young"
     genre = "Rock"
     inType = "CD"    
     mux.add_songs_in_path(myPath, album, artist, genre, inType)
