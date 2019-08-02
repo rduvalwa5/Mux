6d629acbbb3bc12690480ede6a9c376cb7afc8f0
rduvalwa5 <rduvalwa5@hotmail.com>
2019-08-01 17:53:54 -0700

    added classical music

--- Mux_src/aScript_Add_Songs_in_Path.py
+++ Mux_src/aScript_Add_Songs_in_Path.py
@@ -7,11 +7,11 @@ from Music_Get_Functions import musicGet_Functions
         
 if __name__ == "__main__" :       
     mux = musicGet_Functions(True)
-    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/The Allman Brothers Band/Hittin' the Note"    
-    album = "Hittin' the Note"
-    artist = "The Allman Brothers Band"
-    genre = "Rock"
-    inType = "Download"    
+    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Compilations/Le Chant Du Monde 1983"    
+    album = "Le Chant Du Monde 1983"
+    artist = "Compilations"
+    genre = "Classical"
+    inType = "Vinyl"    
     mux.add_songs_in_path(myPath, album, artist, genre, inType)
 #    mux.add_album(album, artist, genre, inType)
 #    mux.add_artist(artist, genre)
