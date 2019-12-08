607d310cf6a053b976a89ae1f609bdcc893cc064
rduvalwa5 <rduvalwa5@hotmail.com>
2019-12-07 23:16:43 -0800

    update TestResults

--- Mux_src/Test_Results.py
+++ Mux_src/Test_Results.py
@@ -23,27 +23,27 @@ TestGenre    3
 TexMex    218
 '''
 
-cover_count = 622
-songs_count = 11586
-artist_count = 565
-artist_albums_count = 1203
+cover_count = 701
+songs_count = 11674
+artist_count = 567
+artist_albums_count = 1210
 folk_songs = 696
 folk_albums = 53
-get_max_index_artist = 567
-get_max_index_albums = 1211
-get_max_index_songs = 11618
+get_max_index_artist = 569
+get_max_index_albums = 1218
+get_max_index_songs = 11706
 #get_song = (1134, 'OSXAir.home', '/Users/rduvalwa2/Music/iTunes/iTunes Music/Music', 'Chuck Berry', 'The Best of Chuck Berry', '08 Johnny B. Goode.mp3', 'Rock', 'Vinyl', 1)
 get_song = ((2389, 'OSXAir.home', '/Users/rduvalwa2/Music/iTunes/iTunes Music/Music', 'Grateful Dead', 'The Grateful Dead (Skull & Roses)', '09 Johnny B. Goode.mp3', 'Rock', 'Download', 1), (4044, 'OSXAir.home', '/Users/rduvalwa2/Music/iTunes/iTunes Music/Music', 'Chuck Berry', 'The Best of Chuck Berry', '08 Johnny B. Goode.mp3', 'Rock', 'Vinyl', 1))
-get_album = (188, 'Ten Years After', 'A Space In Time', 'Blues', 'Download', None, None)
+get_album = (188, 'Ten Years After', 'A Space In Time', 'Blues', 'Download', 'iTunesImages.jpg', 631)
 get_artist = (95, 'Ten Years After', 'Blues')
-get_artist_albums = ((185, 'Ten Years After', 'Stonedhenge (Re-Presents)', 'Blues', 'Download', None, None), (186, 'Ten Years After', 'Recorded Live', 'Blues', 'Download', None, None), (187, 'Ten Years After', 'Undead (Remastered) [Live]', 'Blues', 'Download', None, None), (188, 'Ten Years After', 'A Space In Time', 'Blues', 'Download', None, None))
+get_artist_albums = ((185, 'Ten Years After', 'Stonedhenge (Re-Presents)', 'Blues', 'Download','iTunesImages.jpg', 631), (186, 'Ten Years After', 'Recorded Live', 'Blues', 'Download','iTunesImages.jpg', 631), (187, 'Ten Years After', 'Undead (Remastered) [Live]', 'Blues', 'Download','iTunesImages.jpg', 631), (188, 'Ten Years After', 'A Space In Time', 'Blues', 'Download','iTunesImages.jpg', 631))
 get_artist_albums_songs = (("03 I'd Love to Change the World.m4p",), ('02 Here They Come.m4p',), ("05 Baby Won't You Let Me Rock 'N' Roll You.m4p",), ('01 One of These Days.m4p',), ('10 Uncle Jam.m4p',), ('04 Over the Hill.m4p',), ('07 Let the Sky Fall.m4p',), ("09 I've Been There Too.m4p",), ('06 Once There Was a Time.m4p',), ('08 Hard Monkeys.m4p',))
 
 get_artist_songs = (('08 Johnny B. Goode.mp3', 'The Best of Chuck Berry'), ('02 Roll Over Beethoven.mp3', 'The Best of Chuck Berry'), ('10 Sweet Little Sixteen.mp3', 'The Best of Chuck Berry'), ('09 Brown Eyed Handsome Man.mp3', 'The Best of Chuck Berry'), ('07 Maybelline.mp3', 'The Best of Chuck Berry'), ('03 Rock And Roll Music.mp3', 'The Best of Chuck Berry'), ('05 No Particular Place To Go.mp3', 'The Best of Chuck Berry'), ('06 My Ding A Ling.mp3', 'The Best of Chuck Berry'), ('01 School Days.mp3', 'The Best of Chuck Berry'), ("04 Reelin' and Rockin'.mp3", 'The Best of Chuck Berry')) 
-genreList = [('Alternative', 11), ('BlueGrass', 179), ('Blues', 803), ('Classical', 65), ('Country', 1105), \
+genreList = [('Alternative', 11), ('BlueGrass', 179), ('Blues', 803), ('Classical', 115), ('Country', 1105), \
             ('Easy Listening', 28), ('Folk', 696), ('Holiday', 0), ('Jazz', 904), ('Latino', 0), ('Pop', 716), ('Regae', 23), \
-            ('Rock', 6346), ('RockaBilly', 32), ('Soul', 25), ('Talk', 1), ('TestGenre', 3), ('TexMex', 218), \
+            ('Rock', 6346), (11586,'RockaBilly', 32), ('Soul', 25), ('Talk', 1), ('TestGenre', 3), ('TexMex', 218), \
             ('Traditional', 0), ('World', 0)]
-typeList = [('CD', 6257), ('Download', 3465), ('Tape', 24), ('TestType', 3), ('Vinyl', 1837)]
+typeList = [('CD', 6261), ('Download', 3517), ('Tape',56), ('TestType', 3), ('Vinyl', 1837)]
 genresList = (('Rock', 1), ('Alternative', 7), ('BlueGrass', 8), ('Blues', 9), ('Classical', 10), ('Country', 11), ('Folk', 12), ('Holiday', 13), ('Jazz', 14), ('Latino', 15), ('Pop', 16), ('Regae', 17), ('RockaBilly', 18), ('Soul', 19), ('Talk', 20), ('TestGenre', 21), ('TexMex', 22), ('Traditional', 23), ('World', 24), ('NewGenre', 25), ('Easy Listening', 26), ('Classic', 27),('R&B', 28))
 
