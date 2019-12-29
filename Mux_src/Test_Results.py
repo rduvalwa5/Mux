f92e2c436b6108723ae6aa05eb5b8a710b989d64
rduvalwa5 <rduvalwa5@hotmail.com>
2019-12-29 12:17:38 -0800

    add song

--- Mux_src/Test_Results.py
+++ Mux_src/Test_Results.py
@@ -24,14 +24,14 @@ TexMex    218
 '''
 
 cover_count = 779
-songs_count = 11801
-artist_count = 569
-artist_albums_count = 1218
+songs_count = 11802
+artist_count = 570
+artist_albums_count = 1219
 folk_songs = 696
 folk_albums = 53
-get_max_index_artist = 571
-get_max_index_albums = 1226
-get_max_index_songs = 11833
+get_max_index_artist = 572
+get_max_index_albums = 1227
+get_max_index_songs = 11834
 #get_song = (1134, 'OSXAir.home', '/Users/rduvalwa2/Music/iTunes/iTunes Music/Music', 'Chuck Berry', 'The Best of Chuck Berry', '08 Johnny B. Goode.mp3', 'Rock', 'Vinyl', 1)
 get_song = ((2389, 'OSXAir.home', '/Users/rduvalwa2/Music/iTunes/iTunes Music/Music', 'Grateful Dead', 'The Grateful Dead (Skull & Roses)', '09 Johnny B. Goode.mp3', 'Rock', 'Download', 1), (4044, 'OSXAir.home', '/Users/rduvalwa2/Music/iTunes/iTunes Music/Music', 'Chuck Berry', 'The Best of Chuck Berry', '08 Johnny B. Goode.mp3', 'Rock', 'Vinyl', 1))
 get_album = (188, 'Ten Years After', 'A Space In Time', 'Blues', 'Download', 'iTunesImages.jpg', 631)
@@ -40,10 +40,10 @@ get_artist_albums = ((185, 'Ten Years After', 'Stonedhenge (Re-Presents)', 'Blue
 get_artist_albums_songs = (("03 I'd Love to Change the World.m4p",), ('02 Here They Come.m4p',), ("05 Baby Won't You Let Me Rock 'N' Roll You.m4p",), ('01 One of These Days.m4p',), ('10 Uncle Jam.m4p',), ('04 Over the Hill.m4p',), ('07 Let the Sky Fall.m4p',), ("09 I've Been There Too.m4p",), ('06 Once There Was a Time.m4p',), ('08 Hard Monkeys.m4p',))
 
 get_artist_songs = (('08 Johnny B. Goode.mp3', 'The Best of Chuck Berry'), ('02 Roll Over Beethoven.mp3', 'The Best of Chuck Berry'), ('10 Sweet Little Sixteen.mp3', 'The Best of Chuck Berry'), ('09 Brown Eyed Handsome Man.mp3', 'The Best of Chuck Berry'), ('07 Maybelline.mp3', 'The Best of Chuck Berry'), ('03 Rock And Roll Music.mp3', 'The Best of Chuck Berry'), ('05 No Particular Place To Go.mp3', 'The Best of Chuck Berry'), ('06 My Ding A Ling.mp3', 'The Best of Chuck Berry'), ('01 School Days.mp3', 'The Best of Chuck Berry'), ("04 Reelin' and Rockin'.mp3", 'The Best of Chuck Berry')) 
-genreList = [('Alternative', 11), ('BlueGrass', 179), ('Blues', 803), ('Classical', 115), ('Country', 1137), \
+genreList = [('Alternative', 11), ('BlueGrass', 179), ('Blues', 803), ('Classical', 115), ('Country', 1138), \
             ('Easy Listening', 28), ('Folk', 696), ('Holiday', 0), ('Jazz', 912), ('Latino', 0), ('Pop', 716), ('Regae', 23), \
             ('Rock', 6388), ('RockaBilly', 32), ('Soul', 25), ('Talk', 1), ('TestGenre', 3), ('TexMex', 218), \
             ('Traditional', 0), ('World', 0)]
-typeList = [('CD', 6261), ('Download', 3599), ('Tape',101), ('TestType', 3), ('Vinyl', 1837)]
+typeList = [('CD', 6261), ('Download', 3600), ('Tape',101), ('TestType', 3), ('Vinyl', 1837)]
 genresList = (('Rock', 1), ('Alternative', 7), ('BlueGrass', 8), ('Blues', 9), ('Classical', 10), ('Country', 11), ('Folk', 12), ('Holiday', 13), ('Jazz', 14), ('Latino', 15), ('Pop', 16), ('Regae', 17), ('RockaBilly', 18), ('Soul', 19), ('Talk', 20), ('TestGenre', 21), ('TexMex', 22), ('Traditional', 23), ('World', 24), ('NewGenre', 25), ('Easy Listening', 26), ('Classic', 27),('R&B', 28))
 
--- Mux_src/aScript_Add_Songs_in_Path.py
+++ Mux_src/aScript_Add_Songs_in_Path.py
@@ -7,11 +7,11 @@ from Music_Get_Functions import musicGet_Functions
         
 if __name__ == "__main__" :       
     mux = musicGet_Functions(True)
-    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Media/Music/Weather Report/Weather Report"    
-    album = "Weather Report"
-    artist = "Weather Report"
-    genre = "Jazz"
+    myPath = "/Users/rduvalwa2/Music/iTunes/iTunes Music/Music/Pat Benatar/Pat Benatar Greatest Hits"    
+    album = "Pat Benatar Greatest Hits"
+    artist = "Pat Benatar"
+    genre = "Country"
     inType = "Download"    
-#    mux.add_songs_in_path(myPath, album, artist, genre, inType)
-    mux.add_album(album, artist, genre, inType)
+    mux.add_songs_in_path(myPath, album, artist, genre, inType)
+#    mux.add_album(album, artist, genre, inType)
 #    mux.add_artist(artist, genre)
