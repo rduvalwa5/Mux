b0cbb383dce7c312d61aa5873b343b0724d99fb0
rduvalwa2 <rduvalwa2@osxair.hsd1.wa.comcast.net>
2021-01-04 21:55:23 -0800

    add album covers new from vinyl

--- Mux_src/aScript_Add_Songs_in_Path.py
+++ Mux_src/aScript_Add_Songs_in_Path.py
@@ -7,11 +7,11 @@ from Music_Get_Functions import musicGet_Functions
         
 if __name__ == "__main__" :       
     mux = musicGet_Functions(True)
-    myPath = "/Users/rduvalwa2/Music/Music/Media.localized/Jaco Pastorius/JACO Original Soundtrack"    
-    album = "JACO Original Soundtrack"
-    artist = "Jaco Pastorius"
-    genre = "Jazz"
-    inType = "Download"    
-    mux.add_songs_in_path(myPath, album, artist, genre, inType)
-#   mux.add_album(album, artist, genre, inType)
+    myPath = "/Users/rduvalwa2/Music/Music/Media.localized/Sir Douglas Quintet/Doug Sahm Live Austin TX"    
+    album = "Doug Sahm Live Austin TX"
+    artist = "Sir Douglas Quintet"
+    genre = "TexMex"
+    inType = "Vinyl"    
+#    mux.add_songs_in_path(myPath, album, artist, genre, inType)
+    mux.add_album(album, artist, genre, inType)
 #    mux.add_artist(artist, genre)
